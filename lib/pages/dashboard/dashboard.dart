532bba4393edde3467b117a8323b2edba37c8a58
BlockCat <zinoonomiwo@gmail.com>
2023-05-29 23:18:27 +0200

    load cat on dash

--- lib/pages/dashboard/dashboard.dart
+++ lib/pages/dashboard/dashboard.dart
@@ -1,18 +1,35 @@
-import 'package:cat_budget/pages/dashboard/dashboard_bloc.dart';
+import 'package:cat_budget/data/database/database.dart';
+import 'package:cat_budget/pages/dashboard/edit_categories/edit_category_state.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return BlocProvider(create: (_) => DashboardBloc(), child: _HomePage());
-  }
+  State<StatefulWidget> createState() => _DashboardPageState();
 }
 
-class _HomePage extends StatelessWidget {
+class _DashboardPageState extends State<DashboardPage> {
+  DateTime _selectedDate = DateTime.now();
+  EditCategoryState state = const EditCategoryState(categories: []);
+
+  @override
+  void initState() {
+    super.initState();
+    loadCategories();
+  }
+
+  loadCategories() async {
+    final database = GetIt.I.get<MainDatabase>();
+    final categories = await database.categoryDao.loadCategories();
+
+    setState(() {
+      state = EditCategoryState(categories: categories);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,119 +39,28 @@ class _HomePage extends StatelessWidget {
           IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () async {
-              var result = await context.push('/categories/edit');
-
-              print("Result: $result");
+              await context.push('/categories/edit');
             },
           )
         ],
       ),
       body: ListView(
-        children: const [
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('Other category'),
-            subtitle: Text('total budget account: \$0.00'),
-          ),
-          Divider(),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-          SizedBox(
-            height: 40,
-            child: Placeholder(),
-          ),
-        ],
-      ),
+          children: state.categories
+              .expand((e) => [
+                    ListTile(
+                      title: Text(e.group.group.name.value),
+                      subtitle: Text(e.group.group.description.value ?? ''),
+                      tileColor: Colors.red[300],
+                    ),
+                    const Divider(),
+                    ...e.children.map(
+                      (e) => ListTile(
+                        title: Text(e.category.name.value),
+                        subtitle: Text(e.category.description.value ?? ''),
+                      ),
+                    ),
+                  ])
+              .toList()),
     );
   }
 }
